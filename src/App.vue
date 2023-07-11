470fd207e2017bb68e9f35a9b2147e16f9f7414d
Pankens <Sihrkkpower1@gmail.com>
2023-07-11 12:08:44 +0200

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <RouterView class="content" />
+</template>
+
+<style scoped>
+.content {
+  max-width: 100%;
+}
+</style>
